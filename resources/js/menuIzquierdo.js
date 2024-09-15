84f873ca00874ae9be97f4137861de13445dc19b
Andres-Adrian-Martin-Canto <L19390732@chetumal.tecnm.mx>
2024-09-14 21:09:38 -0500

    Hover del menu de navegacion Costos Fijos,Variables e ingresos

--- resources/js/menuIzquierdo.js
+++ resources/js/menuIzquierdo.js
@@ -3,16 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
     // Asignarle el evento
     a.forEach((ae) => {
         // Se agregan los eventos.
-        ae.onmouseover = function () {
+        ae.onmouseenter = function () {
             ae.querySelector('span').classList.replace('bg-gray-700', 'bg-green-500');
-            ae.querySelector('h3').classList.add('text-green-500');
+            ae.querySelector('h3').classList.replace('text-gray-400', 'text-white');
+            console.log(ae.querySelector('h3'));
             ae.querySelector('svg').classList.replace('dark:text-gray-400', 'dark:text-white');
 
         };
 
-        ae.onmouseout = function () {
+        ae.onmouseleave = function () {
             ae.querySelector('span').classList.replace('bg-green-500', 'bg-gray-700');
-            ae.querySelector('h3').classList.replace('text-green-500', 'text-gray-400');
+            ae.querySelector('h3').classList.replace('text-white', 'text-gray-400');
             ae.querySelector('svg').classList.replace('dark:text-white', 'dark:text-gray-400');
         };
     });
--- resources/views/plan_financiero/menuIzquierdo.blade.php
+++ resources/views/plan_financiero/menuIzquierdo.blade.php
@@ -1,4 +1,4 @@
-<ol class="relative text-gray-500 border-s border-gray-200 dark:border-gray-700 dark:text-gray-400 ">
+<ol class="relative  border-s border-gray-200 dark:border-gray-700  ">
     <a href="{{ route('plan_de_negocio.costo_fijo.index', $plan_de_negocio) }}">
         <div class="mb-10 ">
             <li class="ms-6 ">
@@ -10,7 +10,7 @@
                             d="M18 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM6.5 3a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5ZM3.014 13.021l.157-.625A3.427 3.427 0 0 1 6.5 9.571a3.426 3.426 0 0 1 3.322 2.805l.159.622-6.967.023ZM16 12h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Z" />
                     </svg>
                 </span>
-                <h3 class="font-medium leading-tight pt-1.5">Costos Fijos</h3>
+                <h3 class="text-gray-400 leading-tight pt-1.5">Costos Fijos</h3>
             </li>
         </div>
     </a>
@@ -25,7 +25,7 @@
                             d="M18 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM6.5 3a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5ZM3.014 13.021l.157-.625A3.427 3.427 0 0 1 6.5 9.571a3.426 3.426 0 0 1 3.322 2.805l.159.622-6.967.023ZM16 12h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Z" />
                     </svg>
                 </span>
-                <h3 class="font-medium leading-tight pt-1.5">Costos Variables</h3>
+                <h3 class="text-gray-400 leading-tight pt-1.5">Costos Variables</h3>
             </li>
         </div>
     </a>
@@ -39,7 +39,7 @@
                         d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
                 </svg>
             </span>
-            <h3 class="font-medium leading-tight pt-1.5">Ingresos</h3>
+            <h3 class="text-gray-400 leading-tight pt-1.5">Ingresos</h3>
         </li>
     </a>
     <a href="{{route('plan_de_negocio.estadisticas.index', $plan_de_negocio)}}">
@@ -52,7 +52,7 @@
                         d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2ZM7 2h4v3H7V2Zm5.7 8.289-3.975 3.857a1 1 0 0 1-1.393 0L5.3 12.182a1.002 1.002 0 1 1 1.4-1.436l1.328 1.289 3.28-3.181a1 1 0 1 1 1.392 1.435Z" />
                 </svg>
             </span>
-            <h3 class="font-medium leading-tight lg:text-sm lg:pt-1 2xl:pt-1.5">Proyeccion de un año</h3>
+            <h3 class="text-gray-400 leading-tight lg:text-sm lg:pt-1 2xl:pt-1.5">Proyeccion de un año</h3>
         </li>
     </a>
 </ol>
